d66ee62c505b19e2ce751f010926c56c7c2ee246
it-com-sasha <in-mg@ukr.net>
2020-05-03 23:32:03 +0300

    Расширяем приложение

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseField(models.Model):
+	title = models.CharField('заголовок', max_length=150)
+	description = models.TextField('текст', blank=True)
+	slug = models.SlugField('чпу', db_index=True, unique=True)
+
+	class Meta:
+		abstract = True
+
+	
+	def __str__(self):
+		return self.title
+
+
+
+
+class Page(BaseField):
+	category = models.ManyToManyField('Category', verbose_name='Категория')
+	menu = models.ManyToManyField('Menu', verbose_name='Меню')
+
+	class Meta:
+		verbose_name = "Страница"
+		verbose_name_plural = "Страници"
+		
+
+
+class Category(BaseField):
+
+	class Meta:
+		verbose_name = "Категория"
+		verbose_name_plural = "Категории"
+
+
+
+class Menu(BaseField):
+	slug = None
+	
+	class Meta:
+		verbose_name = "Меню"
+		verbose_name_plural = "Меню"
